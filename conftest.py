b034a23cc7b28c05f4cbea9b217794ee53fb592a
Nast8065 <n9069968806n@yandex.ru>
2025-09-23 19:46:35 +0700

    Update conftest.py
    Внесение изменений

--- conftest.py
+++ conftest.py
@@ -6,14 +6,18 @@ logger = logging.getLogger(__name__)
 
 @pytest.fixture()
 def courier():
-    courier_create = Courier().courier_registration_in_the_system_and_get_courier_data()
-    courier_login = Courier().courier_login_in_the_system_and_get_id_courier(courier_create["data"])
+    courier_instance = Courier()  # Создаем один экземпляр класса Courier
+    courier_create = courier_instance.courier_registration_in_the_system_and_get_courier_data()
+    courier_login = courier_instance.courier_login_in_the_system_and_get_id_courier(courier_create["data"])
     yield courier_create
-    Courier().courier_subsequent_deletion(courier_login["id"])
+    courier_instance.courier_subsequent_deletion(courier_login["id"])
+
 
 @pytest.fixture()
 def courier_delete():
-    courier_create = Courier().courier_registration_in_the_system_and_get_courier_data()
+    courier_instance = Courier()  # Создаем один экземпляр класса Courier
+    courier_create = courier_instance.courier_registration_in_the_system_and_get_courier_data()
     # logger.info(courier_create['data'])  # если необходимо логировать, тогда убрать print
-    courier_login = Courier().courier_login_in_the_system_and_get_id_courier(courier_create["data"])
-    yield courier_login
+    courier_login = courier_instance.courier_login_in_the_system_and_get_id_courier(courier_create["data"])
+    return courier_login   # Используем return вместо yield
+
