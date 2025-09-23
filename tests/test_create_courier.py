4ad5d7c311168a4904ac1ac2527dbbb9dbc62570
Nast8065 <n9069968806n@yandex.ru>
2025-09-23 21:19:32 +0700

    Update test_create_courier.py

--- tests/test_create_courier.py
+++ tests/test_create_courier.py
@@ -9,19 +9,19 @@ class TestCreateCourier:
 
     @allure.title('Проверка создания нового курьера')
     @allure.description('Отправляем запрос на создание курьера, проверяем ответ и удаляем созданного курьера')
-    def test_registration_courier_success(self):
+    def test_registration_courier_success(self, courier_fixture):
         with allure.step("Генерируем валидные данные для курьера"):
             courier_data = DataCourier.valid_data_login
         with allure.step("Отправляем запрос на создание курьера"):
-            response = requests.post(f'{Urls.Yandex_Scooter_URL}{Endpoints.create_courier}', data=courier_data)
+            response = requests.post(f'{Urls.Yandex_scooter_URL}{Endpoints.create_courier}', data=courier_data)
         with allure.step("Проверяем успешный статус и ответ от сервера"):
-            assert response.status_code ==201
+            assert response.status_code == 201
             assert response.text == '{"ok":true}'
         with allure.step("Логинимся для получения ID"):
-            login_resp = requests.post(f'{Urls.Yandex_Scooter_URL}{Endpoints.login_courier}', data=courier_data)
+            login_resp = requests.post(f'{Urls.Yandex_scooter_URL}{Endpoints.login_courier}', data=courier_data)
+            assert login_resp.status_code == 200  # Проверка успешного логина
             courier_id = login_resp.json().get("id")
-        with allure.step("Удаляем созданного курьера"):
-            requests.delete(f'{Urls.Yandex_Scooter_URL}{Endpoints.delete_courier}{courier_id}')
+            assert courier_id == courier_fixture  # Сравниваем полученный ID с ID из фикстуры
 
     @allure.title('Проверка ошибки при создании двух одинаковых курьеров')
     @allure.description('Отправляем повторный запрос на создание курьера, проверяем ответ и удаляем курьера')
@@ -29,17 +29,17 @@ class TestCreateCourier:
         with allure.step("Генерируем валидные данные для курьера"):
             courier_data = DataCourier.valid_data_login
         with allure.step("Первый запрос на создание курьера"):
-            requests.post(f'{Urls.Yandex_Scooter_URL}{Endpoints.create_courier}', data=courier_data)
+            requests.post(f'{Urls.Yandex_scooter_URL}{Endpoints.create_courier}', data=courier_data)
         with allure.step("Второй запрос на создание курьера с теми же данными"):
-            response = requests.post(f'{Urls.Yandex_Scooter_URL}{Endpoints.create_courier}', data=courier_data)
+            response = requests.post(f'{Urls.Yandex_scooter_URL}{Endpoints.create_courier}', data=courier_data)
         with allure.step("Проверяем, что код ответа 409 и присутствует сообщение об ошибке"):
             assert response.status_code == 409
             assert "Этот логин уже используется" in response.text
         with allure.step("Логинимся для удаления курьера"):
-            login_resp = requests.post(f'{Urls.Yandex_Scooter_URL}{Endpoints.login_courier}', data=courier_data)
+            login_resp = requests.post(f'{Urls.Yandex_scooter_URL}{Endpoints.login_courier}', data=courier_data)
             courier_id = login_resp.json().get("id")
         with allure.step("Удаляем курьера"):
-            requests.delete(f'{Urls.Yandex_Scooter_URL}{Endpoints.delete_courier}{courier_id}')
+            requests.delete(f'{Urls.Yandex_scooter_URL}{Endpoints.delete_courier}{courier_id}')
 
     @allure.title('Проверка ошибки при создании курьера без обязательных полей')
     @allure.description('Отправляем запрос без обязательных полей и проверяем ошибку')
@@ -49,7 +49,7 @@ class TestCreateCourier:
     ])
     def test_courier_registration_without_parameters_failed(self, courier_data):
         with allure.step("Отправляем запрос с неполными данными"):
-            response = requests.post(f'{Urls.Yandex_Scooter_URL}{Endpoints.create_courier}', data=courier_data)
+            response = requests.post(f'{Urls.Yandex_scooter_URL}{Endpoints.create_courier}', data=courier_data)
         with allure.step("Проверяем, что получаем ошибку 400 и сообщение о нехватке данных"):
             assert response.status_code == 400
             assert "Недостаточно данных для создания учетной записи" in response.text
