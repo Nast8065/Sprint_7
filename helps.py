c462c055062c7c8f7f02ac8878078d6133667436
Nast8065 <n9069968806n@yandex.ru>
2025-09-23 20:25:36 +0700

    Update helps.py
    Внесены изменения в задания

--- helps.py
+++ helps.py
@@ -7,6 +7,7 @@ from urls import Urls
 
 class CreatingCourier:
     # функция создания курьера с валидными несуществующими данными
+    @step
     @staticmethod
     def creating_courier_fake_data():
         fake = Faker("ru_RU")
@@ -14,28 +15,30 @@ class CreatingCourier:
         password = fake.password()
         firstname = fake.first_name()
         data = {
-            "login": login,
-            "password": password,
-            "firstName": firstname
+            "login": "login",
+            "password": "password",
+            "firstName": "firstname"
         }
 
         return data
 
     # функция создания курьера с не валидными данными без поля "Password"
+    @step
     @staticmethod
     def creating_courier_invalid_data_without_password_field():
         fake = Faker("ru_RU")
         firstname = fake.first_name()
         password = fake.password()
         data = {
-            "login": login,
+            "login": "login",
             "password": "",
-            "firstName": firstname
+            "firstName": "firstname"
         }
 
         return data
 
     # функция создания курьера с не валидными данными без поля "Login"
+    @step
     @staticmethod
     def creating_courier_invalid_data_without_login_field():
         fake = Faker("ru_RU")
@@ -43,8 +46,8 @@ class CreatingCourier:
         firstname = fake.first_name()
         data = {
             "login": "",
-            "password": password,
-            "firstName": firstname
+            "password": "password",
+            "firstName": "firstname"
         }
 
         return data
@@ -71,21 +74,24 @@ class Courier:
 
     # функция регистрации в системе с возвратом ответа и данных курьера
     @staticmethod
+    @step("Регистрация курьера в системе")
     def courier_registration_in_the_system_and_get_courier_data():
         data = CreatingCourier.generating_fake_valid_data_to_create_courier()
-        response = requests.post(f'{Urls.Yandex_Scooter_URL}{Endpoints.create_courier}', data=data)
+        response = requests.post(f'{Urls.Yandex_scooter_URL}{Endpoints.create_courier}', data=data)
         return {"response_text": response.text, "status_code": response.status_code, "data": data}
 
     # функция логина в системе с возвратом ответа и id курьера
     @staticmethod
+    @step("Логин курьера в системе")
     def courier_login_in_the_system_and_get_id_courier(data):
-        response = requests.post(f'{Urls.Yandex_Scooter_URL}{Endpoints.login_courier}', data=data)
+        response = requests.post(f'{Urls.Yandex_scooter_URL}{Endpoints.login_courier}', data=data)
         return {"id": str(response.json()["id"]), "response_text": response.text, "status_code": response.status_code}
 
     # функция удаления курьера
     @staticmethod
+    @step("Удаление курьера")
     def courier_subsequent_deletion(id):
-        response = requests.delete(f'{Urls.Yandex_Scooter_URL}{Endpoints.delete_courier}{id}')
+        response = requests.delete(f'{Urls.Yandex_scooter_URL}{Endpoints.delete_courier}{id}')
         return {"response_text": response.text, "status_code": response.status_code}
 
 
