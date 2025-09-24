11090e25f9d426e81df16247467af201b639dfae
Nast8065 <n9069968806n@yandex.ru>
2025-09-24 23:16:09 +0700

    Update conftest.py
    добавили фикстуру для удаления  курьера

--- conftest.py
+++ conftest.py
@@ -21,3 +21,23 @@ def courier_delete():
     courier_login = courier_instance.courier_login_in_the_system_and_get_id_courier(courier_create["data"])
     return courier_login   # Используем return вместо yield
 
+@pytest.fixture
+def courier_fixture():
+    """Фикстура для создания и удаления курьера."""
+    courier_data = DataCourier.valid_data_login
+    
+    # Создаем курьера
+    response = requests.post(f'{Urls.Yandex_scooter_URL}{Endpoints.create_courier}', data=courier_data)
+    assert response.status_code == 201  # Проверяем успешное создание курьера
+
+    # Получаем ID курьера для дальнейшего использования
+    login_resp = requests.post(f'{Urls.Yandex_scooter_URL}{Endpoints.login_courier}', data=courier_data)
+    assert login_resp.status_code == 200  # Проверяем успешный логин
+    courier_id = login_resp.json().get("id")
+
+    yield courier_id  # Возвращаем ID курьера для использования в тестах
+
+    # Удаляем курьера после завершения теста
+    requests.delete(f'{Urls.Yandex_scooter_URL}{Endpoints.delete_courier}{courier_id}')
+
+
